65f44875a08cd487a9dbbdb061f8bf64908d5d97
Stamp9 <383990832@qq.com>
2025-06-05 09:19:44 +0000

    fix: errorboundry

--- webapp/frontend/src/main.tsx
+++ webapp/frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-import ErrorBoundary from "./components/common/ErrorBoundary.tsx";
 import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
